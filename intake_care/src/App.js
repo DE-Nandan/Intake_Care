61cc52042bc40423b09c4794822833e27a1e0a27
Nandan Kumar <nandankmrde@gmail.com>
2022-06-06 06:20:04 +0530

    frologdone

--- intake_care/src/App.js
+++ intake_care/src/App.js
@@ -7,8 +7,8 @@ function App() {
       
         <div className='App'>
       {/* <Homepage/> */}
-       {/* <Login/> */}
-      <Register/> 
+       <Login/>
+      {/* <Register/>  */}
       </div>
   );
 }
--- intake_care/src/components/login/Login.jsx
+++ intake_care/src/components/login/Login.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React , {useState} from 'react'
 import './login.css'
 const Login = () => {
+
+    const [user , setUser] = useState({
+      
+        email:"",
+        password:"",
+       
+    })
+
+    const handleChange = e => {
+        
+        const {name , value} = e.target
+        console.log(name,value);
+        setUser({
+            ...user,
+            [name] : value
+        })
+    }
+
+
   return (
     <div className="login">
+    {
+        console.log("User" , user)
+    }
         <h1>Login</h1>
-        <input type="text" placeholder='Type your name' />
-        <input type="password" placeholder='Password' />
+        <input type="emil" name = "email" value = {user.email} onChange={handleChange} placeholder='Type your email' />
+        <input type="password" name = "password" value = {user.password} onChange={handleChange} placeholder='Password' />
         <div className="button">Login</div>
          <div>or</div>
          <div className="button">Register</div>
