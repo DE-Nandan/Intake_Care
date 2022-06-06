3929903a5872a1b3ad79c767597df264d079aff2
Nandan Kumar <nandankmrde@gmail.com>
2022-06-06 06:06:16 +0530

    advancing

--- intake_care/src/components/register/Register.jsx
+++ intake_care/src/components/register/Register.jsx
@@ -9,13 +9,26 @@ const Register = () => {
         password:"",
         reEnterPassword:"",
     })
+
+    const handleChange = e => {
+        
+        const {name , value} = e.target
+        console.log(name,value);
+        setUser({
+            ...user,
+            [name] : value
+        })
+    }
   return (
     <div className="register">
+    {
+        console.log("User",user)
+    }
     <h1>Register</h1>
-    <input type="text" placeholder='Type your name' />
-    <input type="text" placeholder='Type your Email' />
-    <input type="password" placeholder='Password' />
-    <input type="password" placeholder='Re-enter Password' />
+    <input type="text" name = "name" value = {user.name} placeholder='Type your name' onChange={ handleChange} />
+    <input type="text" name = "email" value = {user.email} placeholder='Type your Email' onChange={handleChange}/>
+    <input type="password" name = "password" value = {user.password} placeholder='Password'onChange={handleChange} />
+    <input type="password" name = "reEnterPassword" value = {user.reEnterPassword} placeholder='Re-enter Password' onChange={handleChange} />
     <div className="button">Register</div>
      <div>or</div>
      <div className="button">Login</div>
