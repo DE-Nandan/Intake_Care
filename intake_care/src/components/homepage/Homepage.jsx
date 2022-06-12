be6c5c0f86a6691c0e8a473178f79d496d59ae4a
Nandan Kumar <nandankmrde@gmail.com>
2022-06-12 06:10:13 +0530

    logsigndone

--- intake_care/src/components/homepage/Homepage.jsx
+++ intake_care/src/components/homepage/Homepage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './homepage.css'
+import Navbar from './navbar/Navbar'
 const Homepage = ({obj ,setLoginUser}) => {
   console.log(obj)
   return (
     <div className="homepage">
+    <Navbar/>
       <h1>Hello {obj.name} {obj.email} ans pass is {(obj.password)*2} {setLoginUser.setLoginUser} </h1>
       <div className="button" onClick={() => setLoginUser({})}>Logout</div>
     </div>
