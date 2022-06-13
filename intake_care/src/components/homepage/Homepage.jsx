b47e5af7f92718166089deda10d10bdc7571f9c6
Nandan Kumar <nandankmrde@gmail.com>
2022-06-14 03:06:54 +0530

    navadded

--- intake_care/src/components/homepage/Homepage.jsx
+++ intake_care/src/components/homepage/Homepage.jsx
@@ -4,11 +4,14 @@ import Navbar from './navbar/Navbar'
 const Homepage = ({obj ,setLoginUser}) => {
   console.log(obj)
   return (
+    <>
+    <Navbar obj = {obj}/>
     <div className="homepage">
-    <Navbar/>
+    
       <h1>Hello {obj.name} {obj.email} ans pass is {(obj.password)*2} {setLoginUser.setLoginUser} </h1>
       <div className="button" onClick={() => setLoginUser({})}>Logout</div>
     </div>
+    </>
   )
 }
 
